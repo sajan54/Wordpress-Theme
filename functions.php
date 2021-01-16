13ae85ca27a53959b288be55f4f5fcae07625adb
sajan54 <sajanmdhr54@gmail.com>
2021-01-16 20:47:17 +0545

    Worked on footer and added widgets

--- functions.php
+++ functions.php
@@ -4,4 +4,5 @@ require_once('lib/enqueue-assets.php');
 require_once('lib/theme-support.php');
 require_once('lib/menu.php');
 require_once('lib/customizer/index.php');
+require_once('lib/widgets.php');
 ?>
